1eeb27365aaf51161169b4167b8a546fe07ff390
brandondunc94 <brandon.douglas.duncan@gmail.com>
2021-05-11 14:37:02 -0700

    Updated root urls to directly point to admin page

--- dnd-server/dnd/urls.py
+++ dnd-server/dnd/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from room.views import index, room
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('play/<room_code>', room),
+    path('', admin.site.urls)
 ]
