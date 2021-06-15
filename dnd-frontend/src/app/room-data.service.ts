087184527089f3d2a4f23b1fa9a2b27f16caad1c
brandondunc94 <brandon.douglas.duncan@gmail.com>
2021-06-14 20:22:18 -0700

    Added canvas functionality - users can draw on the map/canvas and upload the image to the server for other players to see

--- dnd-frontend/src/app/room-data.service.ts
+++ dnd-frontend/src/app/room-data.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { Subject } from "rxjs";
 import { map } from 'rxjs/operators';
 import { RoomWebsocketService } from "./room-websocket.service";
 import { environment } from '../environments/environment';
-
+import { HttpClient } from '@angular/common/http';
+ 
 const CHAT_URL = environment.websocketURL;
 
 export interface RoomData {
@@ -34,12 +35,25 @@ export class Character {
 @Injectable()
 export class RoomService {
   public roomData: Subject<RoomData>;
+  public CANVAS_URL = environment.canvasImageUpload;
 
-  constructor(wsService: RoomWebsocketService) { // Receive room data from websocket
+  constructor(wsService: RoomWebsocketService, private http: HttpClient) { // Receive room data from websocket
     this.roomData = <Subject<RoomData>>wsService.connect(CHAT_URL).pipe(map(
-      (response: MessageEvent): RoomData => { let data = JSON.parse(response.data);
+      (response: MessageEvent): RoomData => { 
+        let data = JSON.parse(response.data);
         return data.payload.roomData;
       }
     ));
   }
+
+  // POST method for when a player uploads a new version of the room's canvas
+  uploadCanvasImage(image: Blob, roomCode: string) {
+    const formData: FormData = new FormData(); // Create form for POST call and attach image blob
+    formData.append('canvasImageKey', image, roomCode);
+
+    this.http.post(this.CANVAS_URL + roomCode + '/', formData).subscribe(response => { // Make POST call to send latest canvas
+      console.log(response);
+    });
+  }
+
 }
