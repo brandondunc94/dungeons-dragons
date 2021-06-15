a3452befd3017cc3c1120bab50e48175c0889586
brandondunc94 <brandon.douglas.duncan@gmail.com>
2021-06-14 20:23:07 -0700

    Added some comments and updated the image file key in the request to upload a new canvas image

--- dnd-server/game/views.py
+++ dnd-server/game/views.py
@@ -7,9 +7,9 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def upload_map_canvas(request, roomCode=1):
     if(request.method == 'POST'):
-        myFile = request.FILES['fileKey']
-        myFile.name = roomCode + '.png'
-        game = Game.objects.get(roomCode=roomCode)
+        myFile = request.FILES['canvasImageKey']
+        myFile.name = roomCode + '.png' #Image name will be the roomCode + .png (Ex. 15.png)
+        game = Game.objects.get(roomCode=roomCode) #Retrieve game the matches the room code
         game.backgroundCanvas =  myFile
         game.save()
         data = {
