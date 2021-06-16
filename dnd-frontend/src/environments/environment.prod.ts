7fd33b7df95801290896f54350d990897a97435a
brandondunc94 <brandon.douglas.duncan@gmail.com>
2021-06-16 16:31:27 -0700

    Updated prod environment path for canvas api upload

--- dnd-frontend/src/environments/environment.prod.ts
+++ dnd-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   websocketURL: 'ws://138.68.4.225:8001/ws/play/1',
-  canvasImageUrl: 'http://138.68.4.225:8001/media/images/',
+  canvasImageUrl: 'http://138.68.4.225/media/images/',
   canvasImageUpload: 'http://138.68.4.225:8001/game/canvas/'
 };
