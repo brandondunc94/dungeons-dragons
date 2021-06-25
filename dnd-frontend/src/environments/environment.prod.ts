1e19415d9bf1f67f64b8b73ebe8739c866913b04
brandondunc94 <brandon.douglas.duncan@gmail.com>
2021-06-24 19:13:11 -0700

    Fixed prod canvas upload url - removed port

--- dnd-frontend/src/environments/environment.prod.ts
+++ dnd-frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   websocketURL: 'ws://138.68.4.225:8001/ws/play/1',
   canvasImageUrl: 'http://138.68.4.225/media/images/',
-  canvasImageUpload: 'http://138.68.4.225:8001/game/canvas/'
+  canvasImageUpload: 'http://138.68.4.225/game/canvas/'
 };
