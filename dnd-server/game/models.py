2c94457fe5905841dcb55a670b2c800d2edb47f2
brandondunc94 <brandon.douglas.duncan@gmail.com>
2021-06-29 14:04:06 -0700

    Added Character model and serializer for api calls

--- dnd-server/game/models.py
+++ dnd-server/game/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 from django.contrib import admin
+from django.db.models.fields import CharField
 from .storage import OverwriteStorage
+import uuid
 
 class Game(models.Model):
     roomCode = models.CharField(primary_key=True, max_length=10, default='')
     backgroundCanvas = models.ImageField(null=True, blank=True, storage=OverwriteStorage(), upload_to='images/', help_text='Latest Map Canvas')
-
 admin.site.register(Game)
+
+class Character(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30)
+    health = models.IntegerField()
+    maxHealth = models.IntegerField()
+    position = models.IntegerField(default=0)
+    type = models.CharField(max_length=30)
+    characterClass = models.CharField(max_length=30)
+admin.site.register(Character)
--- dnd-server/game/serializers.py
+++ dnd-server/game/serializers.py
@@ -1,7 +1,15 @@
-# from rest_framework import serializers
-# from .models import Game
+from rest_framework import serializers
+from .models import Character, Game
 
-# class ImageSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Game
-#         fields = "__all__"
+class CharacterSerializer(serializers.ModelSerializer):
+    game_id = serializers.CharField()
+    class Meta:
+        model = Character
+        fields = ('game_id', 'id', 'name', 'health', 'maxHealth', 'position', 'type', 'characterClass')
+    
+    def create(self, validated_data):
+        game_id = validated_data.pop('game_id') # Get game_id field from serializer and use to lookup game object
+        gameObject = Game.objects.get(roomCode=game_id)
+        newCharacter = Character.objects.create(game=gameObject, **validated_data)
+        return newCharacter
+    
