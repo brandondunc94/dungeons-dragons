79e5125723fc87a181a1ac4bb6d3e35490a59d01
brandondunc94 <brandon.douglas.duncan@gmail.com>
2021-07-02 16:47:33 -0700

    Removd unecessary imports on game views.py

--- dnd-server/game/views.py
+++ dnd-server/game/views.py
@@ -8,9 +8,6 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
-from rest_framework import status
-
-from game import serializers
 
 @csrf_exempt
 def upload_map_canvas(request, roomCode=1):
